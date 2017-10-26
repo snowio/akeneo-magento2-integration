e768fe0fe4ecc8874f30dd6c73c560627ee6fce4
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-26 10:40:57 +0100

    Pass Tests: Provide Imlementation for SimpleProductMapper

--- src/Mapper/SimpleProductMapper.php
+++ src/Mapper/SimpleProductMapper.php
@@ -8,14 +8,31 @@ class SimpleProductMapper
 {
     public static function create(): self
     {
+        $simpleProductMapper = new self;
+        $simpleProductMapper->customAttributeMapper = CustomAttributeMapper::create();
+        return $simpleProductMapper;
     }
 
     public function map(AkeneoProduct $akeneoProduct): Magento2Product
     {
-
+        $magento2Product = Magento2Product::of($akeneoProduct->getSku());
+        $customAttributes = $this->customAttributeMapper->map($akeneoProduct->getAttributeValues());
+        $magento2Product = $magento2Product->withCustomAttributes($customAttributes);
+        return $magento2Product;
     }
 
     public function withCustomAttributeMapper(CustomAttributeMapper $customAttributeMapper): self
     {
+        $result = clone $this;
+        $result->customAttributeMapper = $customAttributeMapper;
+        return $result;
+    }
+
+    /** @var CustomAttributeMapper */
+    private $customAttributeMapper;
+
+    private function __construct()
+    {
+
     }
 }
