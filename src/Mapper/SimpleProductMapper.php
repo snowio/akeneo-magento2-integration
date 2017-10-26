07dfbb577e22028cc44fe66aa6f98a358b208d0d
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-26 10:03:50 +0100

    Add Test case for simple product mapper Update Interface

--- src/Mapper/SimpleProductMapper.php
+++ src/Mapper/SimpleProductMapper.php
@@ -1,7 +1,21 @@
 <?php
 namespace SnowIO\AkeneoMagento2Integration\Mapper;
 
+use SnowIO\AkeneoDataModel\ProductData as AkeneoProduct;
+use SnowIO\Magento2DataModel\ProductData as Magento2Product;
+
 class SimpleProductMapper
 {
+    public static function create(): self
+    {
+    }
 
+    public function map(AkeneoProduct $akeneoProduct): Magento2Product
+    {
+
+    }
+
+    public function withCustomAttributeMapper(CustomAttributeMapper $customAttributeMapper): self
+    {
+    }
 }
--- test/unit/Mapper/SimpleProductMapperTest.php
+++ test/unit/Mapper/SimpleProductMapperTest.php
@@ -1,6 +1,92 @@
 <?php
 
-class SimpleProductMapperTest
+use PHPUnit\Framework\TestCase;
+use SnowIO\AkeneoMagento2Integration\Mapper\CustomAttributeMapper;
+use SnowIO\AkeneoMagento2Integration\Mapper\SimpleProductMapper;
+use SnowIO\AkeneoDataModel\ProductData as AkeneoProduct;
+use SnowIO\Magento2DataModel\CustomAttribute;
+use SnowIO\Magento2DataModel\CustomAttributeSet;
+use SnowIO\Magento2DataModel\ProductData as Magento2ProductData;
+
+class SimpleProductMapperTest extends TestCase
 {
 
+    /**
+     * @dataProvider mapDataProvider
+     */
+    public function testMap(AkeneoProduct $akeneoProduct, Magento2ProductData $expected, SimpleProductMapper $mapper)
+    {
+        $actual = $mapper->map($akeneoProduct);
+        self::assertTrue($actual->eqauls($expected));
+    }
+
+    public function mapDataProvider()
+    {
+        return [
+            'withCustomAttributeMapperSpecified' => [
+                AkeneoProduct::fromJson([
+                    'sku' => 'abc123',
+                    'channel' => 'main',
+                    'categories' => [
+                        ['mens', 't_shirts'],
+                        ['mens', 'trousers'],
+                    ],
+                    'family' => "mens_t_shirts",
+                    'attribute_values' => [
+                        'size' => 'Large',
+                        'sku' => 'abc123',
+                        'status' => '1',
+                        'visibility' =>  '4',
+                        'url_key' => 'abc123',
+                        'price' => [
+                            'gbp' => '40.48',
+                            'euro' => '40.59'
+                        ]
+                    ],
+                    'group' => null,
+                    'localizations' => [],
+                    'enabled' => true,
+                    '@timestamp' => 1508491122,
+                ]),
+                Magento2ProductData::of('abc123')->withCustomAttributes(
+                    CustomAttributeSet::of(
+                        [
+                            CustomAttribute::of('size', 'Large'),
+                            CustomAttribute::of('price', '40.48'),
+                        ]
+                    )
+                ),
+                SimpleProductMapper::create(),
+            ],
+            'withoutCustomAttributeMapperSpecified' => [
+                AkeneoProduct::fromJson([
+                    'sku' => 'abc123',
+                    'channel' => 'main',
+                    'categories' => [
+                        ['mens', 't_shirts'],
+                        ['mens', 'trousers'],
+                    ],
+                    'family' => "mens_t_shirts",
+                    'attribute_values' => [
+                        'size' => 'Large',
+                        'sku' => 'abc123',
+                        'price' => [
+                            'gbp' => '40.48',
+                            'euro' => '40.59'
+                        ]
+                    ],
+                    'group' => null,
+                    'localizations' => [],
+                    'enabled' => true,
+                    '@timestamp' => 1508491122,
+                ]),
+                Magento2ProductData::of('abc123')
+                    ->withCustomAttributes(CustomAttributeSet::of([
+                        CustomAttribute::of('size', 'Large'),
+                    ])),
+                SimpleProductMapper::create()->withCustomAttributeMapper(CustomAttributeMapper::create()
+                    ->withCurrency('gbp'))
+            ],
+        ];
+    }
 }
