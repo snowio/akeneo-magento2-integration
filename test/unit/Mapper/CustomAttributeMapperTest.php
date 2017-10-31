dd0f86f37b12b1c0fe36426c409de08b0f84d3f9
alexwanyoike <wanyoike.alexander@gmail.com>
2017-10-31 10:15:44 +0000

    Improve coverage: Test CustomAttributeMapper With no currency specified

--- test/unit/Mapper/CustomAttributeMapperTest.php
+++ test/unit/Mapper/CustomAttributeMapperTest.php
@@ -9,10 +9,11 @@ use SnowIO\Magento2DataModel\CustomAttributeSet;
 
 class CustomAttributeMapperTest extends TestCase
 {
+    private $akeneoAttributes;
 
-    public function testMap()
+    public function setUp()
     {
-        $akeneoAttributes = AttributeValueSet::fromJson('main', [
+        $this->akeneoAttributes = AttributeValueSet::fromJson('main', [
             'attribute_values' => [
                 'size' => 'Large',
                 'price' => [
@@ -22,13 +23,28 @@ class CustomAttributeMapperTest extends TestCase
                 'weight' => '30',
             ],
         ]);
+    }
+
+    public function testMap()
+    {
         $customAttributeMapper = CustomAttributeMapper::create()->withCurrency('eur');
         $expected = CustomAttributeSet::of([
             CustomAttribute::of('size', 'Large'),
             CustomAttribute::of('price', '37.45'),
             CustomAttribute::of('weight', '30'),
         ]);
-        $actual = $customAttributeMapper->map($akeneoAttributes);
+        $actual = $customAttributeMapper->map($this->akeneoAttributes);
+        self::assertTrue($expected->equals($actual));
+    }
+
+    public function testMapWithoutCurrency()
+    {
+        $customAttributeMapper = CustomAttributeMapper::create();
+        $expected = CustomAttributeSet::of([
+            CustomAttribute::of('size', 'Large'),
+            CustomAttribute::of('weight', '30'),
+        ]);
+        $actual = $customAttributeMapper->map($this->akeneoAttributes);
         self::assertTrue($expected->equals($actual));
     }
 }
