4c4b1a3a20b312ffabcd36b1ae1e6f8ffa7491e8
alexwanyoike <wanyoike.alexander@gmail.com>
2017-11-14 09:37:28 +0000

    Add configurable product mapper

---
+++ src/ConfigurableProductMapper.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+namespace SnowIO\AkeneoMagento2;
+
+use SnowIO\AkeneoDataModel\VariantGroupData;
+use SnowIO\Magento2DataModel\ProductData;
+use SnowIO\Magento2DataModel\ProductStatus;
+use SnowIO\Magento2DataModel\ProductTypeId;
+use SnowIO\Magento2DataModel\ProductVisibility;
+
+class ConfigurableProductMapper
+{
+    public static function create(): self
+    {
+        return new self;
+    }
+
+    public function __invoke(VariantGroupData $akeneoVariantGroup)
+    {
+        $magento2Configurable = ProductData::of(
+            $akeneoVariantGroup->getCode(),
+            $akeneoVariantGroup->getCode()
+        )->withTypeId(ProductTypeId::CONFIGURABLE);
+
+        $family = $akeneoVariantGroup->getFamily();
+        if ($family) {
+            $magento2Configurable = $magento2Configurable->withAttributeSetCode($family);
+        }
+
+        $visibility = ProductVisibility::CATALOG_SEARCH;
+        $customAttributes = ($this->customAttributeMapper)($akeneoVariantGroup->getAttributeValues());
+        return $magento2Configurable
+            ->withVisibility($visibility)
+            ->withCustomAttributes($customAttributes);
+    }
+
+    public function withCustomAttributeMapper(callable $customAttributeMapper): self
+    {
+        $result = clone $this;
+        $result->customAttributeMapper = $customAttributeMapper;
+        return $result;
+    }
+
+    /** @var CustomAttributeMapper */
+    private $customAttributeMapper;
+
+    private function __construct()
+    {
+        $this->customAttributeMapper = CustomAttributeMapper::create();
+    }
+}
---
+++ test/unit/ConfigurableProductMapperTest.php
@@ -0,0 +1,151 @@
+<?php
+declare(strict_types=1);
+namespace SnowIO\AkeneoMagento2\Test;
+
+use PHPUnit\Framework\TestCase;
+use SnowIO\AkeneoDataModel\VariantGroupData as AkeneoConfigurable;
+use SnowIO\AkeneoMagento2\ConfigurableProductMapper;
+use SnowIO\AkeneoMagento2\CustomAttributeMapper;
+use SnowIO\Magento2DataModel\CustomAttribute;
+use SnowIO\Magento2DataModel\CustomAttributeSet;
+use SnowIO\Magento2DataModel\ProductData;
+use SnowIO\Magento2DataModel\ProductTypeId;
+use SnowIO\Magento2DataModel\ProductVisibility;
+
+class ConfigurableProductMapperTest extends TestCase
+{
+    public function testMap()
+    {
+        $akeneoVariant = $this->getAkeneoConfigurableWithProduct();
+        $mapper = ConfigurableProductMapper::create();
+        $actual = $mapper($akeneoVariant);
+        $expected = ProductData::of('abc123', 'abc123')
+            ->withAttributeSetCode('mens_t_shirts')
+            ->withTypeId(ProductTypeId::CONFIGURABLE)
+            ->withVisibility(ProductVisibility::CATALOG_SEARCH)
+            ->withCustomAttributes(CustomAttributeSet::of([
+                CustomAttribute::of('size', 'large'),
+                CustomAttribute::of('color', 'blue'),
+                CustomAttribute::of('product_title', 'ABC 123 Product Variant 1'),
+            ]));
+        self::assertEquals($expected->toJson(), $actual->toJson());
+        self::assertTrue($expected->equals($actual));
+        $akeneoVariant = $this->getAkeneoConfigurableWithoutProduct();
+        $mapper = ConfigurableProductMapper::create();
+        $actual = $mapper($akeneoVariant);
+        $expected = ProductData::of('abc123', 'abc123')
+            ->withTypeId(ProductTypeId::CONFIGURABLE)
+            ->withVisibility(ProductVisibility::CATALOG_SEARCH)
+            ->withCustomAttributes(CustomAttributeSet::of([
+                CustomAttribute::of('size', 'large'),
+                CustomAttribute::of('color', 'blue'),
+                CustomAttribute::of('product_title', 'ABC 123 Product Variant 1'),
+            ]));
+        self::assertEquals($expected->toJson(), $actual->toJson());
+        self::assertTrue($expected->equals($actual));
+    }
+
+
+    public function testMapWithCustomMappers()
+    {
+        $akeneoVariant = $this->getAkeneoConfigurableWithProduct();
+        $mapper = ConfigurableProductMapper::create();
+        $mapper = $mapper->withCustomAttributeMapper(
+            CustomAttributeMapper::create()->withCurrency('gbp')
+        );
+
+        $actual = $mapper($akeneoVariant);
+        $expected = ProductData::of('abc123', 'abc123')
+            ->withAttributeSetCode('mens_t_shirts')
+            ->withTypeId(ProductTypeId::CONFIGURABLE)
+            ->withVisibility(ProductVisibility::CATALOG_SEARCH)
+            ->withCustomAttributes(
+                CustomAttributeSet::of([
+                    CustomAttribute::of('size', 'large'),
+                    CustomAttribute::of('color', 'blue'),
+                    CustomAttribute::of('product_title', 'ABC 123 Product Variant 1'),
+                    CustomAttribute::of('price', '40.48'),
+                ])
+            );
+        self::assertEquals($expected->toJson(), $actual->toJson());
+        self::assertTrue($expected->equals($actual));
+    }
+
+    private function getAkeneoConfigurableWithProduct(): AkeneoConfigurable
+    {
+        return AkeneoConfigurable::fromJson([
+            'code' => 'abc123',
+            'sku' => 'abc123_v1',
+            'products' => [
+                'abc123' => [
+                    'sku' => 'abc123',
+                    'channel' => 'main',
+                    'categories' => [
+                        ['mens', 't_shirts'],
+                        ['mens', 'trousers'],
+                    ],
+                    'family' => "mens_t_shirts",
+                    'attribute_values' => [
+                        'size' => 'Large',
+                        'product_title' => 'ABC 123 Product',
+                        'price' => [
+                            'gbp' => '40.48',
+                            'euro' => '40.59'
+                        ]
+                    ],
+                    'group' => null,
+                    'localizations' => [],
+                    'enabled' => true,
+                    '@timestamp' => 1508491122,
+                ]
+            ],
+            'channel' => 'main',
+            'axis' => ['color', 'size'],
+            'categories' => [
+                ['mens', 't_shirts'],
+                ['mens', 'trousers'],
+            ],
+            'attribute_values' => [
+                'size' => 'large',
+                'color' => 'blue',
+                'product_title' => 'ABC 123 Product Variant 1',
+                'price' => [
+                    'gbp' => '40.48',
+                    'euro' => '40.59'
+                ]
+            ],
+            'group' => null,
+            'localizations' => [],
+            'enabled' => true,
+            '@timestamp' => 1508491122,
+        ]);
+    }
+
+
+    private function getAkeneoConfigurableWithoutProduct(): AkeneoConfigurable
+    {
+        return AkeneoConfigurable::fromJson([
+            'code' => 'abc123',
+            'sku' => 'abc123_v1',
+            'channel' => 'main',
+            'axis' => ['color', 'size'],
+            'categories' => [
+                ['mens', 't_shirts'],
+                ['mens', 'trousers'],
+            ],
+            'attribute_values' => [
+                'size' => 'large',
+                'color' => 'blue',
+                'product_title' => 'ABC 123 Product Variant 1',
+                'price' => [
+                    'gbp' => '40.48',
+                    'euro' => '40.59'
+                ]
+            ],
+            'group' => null,
+            'localizations' => [],
+            'enabled' => true,
+            '@timestamp' => 1508491122,
+        ]);
+    }
+}
